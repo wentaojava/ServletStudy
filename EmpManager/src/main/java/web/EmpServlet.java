2a8383cf4066a6594fbac9d01caf4278d4f2a32d
wentaoxuerong <32974502+wentaoxuerong@users.noreply.github.com>
2017-12-11 22:34:12 +0800

    更新访问路径（一个servelt处理多个请求）

--- EmpManager/src/main/java/web/EmpServlet.java
+++ EmpManager/src/main/java/web/EmpServlet.java
@@ -1,3 +1,6 @@
+/**
+ * 
+ */
 package web;
 
 import java.io.IOException;
@@ -12,13 +15,30 @@ import javax.servlet.http.HttpServletResponse;
 import Dao.EmpDao;
 import Entity.Emp;
 
-public class findEmpServlet extends HttpServlet{
+/**
+ * @author Administrator
+ *查询员工： /find.emp
+ *增加员工： /add.emp
+ */
+public class EmpServlet extends HttpServlet {
 
 	@Override
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//判断访问路径并处理请求
+		String path=request.getServletPath();
+		if("/find.emp".equals(path)){
+			findEmp(request,response);
+		}else if(path.equals("/add.emp")){
+			addEmp(request,response);
+		}else{
+			throw new RuntimeException("暂无此页");
+		}
+	}
+
+	//查询员工
+	protected void findEmp(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		EmpDao dao=new EmpDao();
 		List<Emp> list=dao.findAll();
-		
 		response.setContentType("text/html;charset=utf-8");
 		PrintWriter p=response.getWriter();
 		p.println("<a href='addEmp.html'>新增</a>");
@@ -42,5 +62,20 @@ public class findEmpServlet extends HttpServlet{
 		p.println("</table>");
 		p.close();	
 	}
-
+	
+	//添加员工
+	protected void addEmp(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		req.setCharacterEncoding("utf-8");
+		String empName=req.getParameter("empName");
+		String empJob=req.getParameter("empJob");
+		float empSal=new Integer(req.getParameter("empSal"));
+		Emp emp=new Emp();
+		emp.setName(empName);;
+		emp.setJob(empJob);
+		emp.setSal(empSal);
+		EmpDao e=new EmpDao();
+		e.saveEmp(emp);
+		//重定向
+		res.sendRedirect("find.emp");
+	}
 }

4065b5e30431b2121310b0a838d2ca54ce3b835e
wentaoxuerong <32974502+wentaoxuerong@users.noreply.github.com>
2017-12-11 22:29:14 +0800

    访问路径演示

---
+++ Servlet3/src/main/java/web/HelloServlet.java
@@ -0,0 +1,22 @@
+package web;
+
+import java.io.IOException;
+import java.io.Serializable;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet {
+
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		//获取访问路径
+		System.out.println(req.getContextPath());
+		System.out.println(req.getServletPath());
+		System.out.println(req.getRequestURI());
+		System.out.println(req.getRequestURL());
+	}
+
+}

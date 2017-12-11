e62d92206071f3864d8b0aee6542a09a76bb49fd
wentaoxuerong <32974502+wentaoxuerong@users.noreply.github.com>
2017-12-11 23:07:32 +0800

    servlet生命周期演示

---
+++ servlet4/src/main/java/web/DemoServlet.java
@@ -0,0 +1,34 @@
+package web;
+
+import java.io.IOException;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class DemoServlet extends HttpServlet {
+	
+	public DemoServlet(){
+		System.out.println("实例化DemoServlet");
+	}
+
+	@Override
+	public void destroy() {
+		super.destroy();
+		System.out.println("销毁DemoServlet");
+	}
+
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		super.init(config);
+		System.out.println("初始化DemoServlet");
+	}
+
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		System.out.println("调用DemoServlet");
+	}
+
+}

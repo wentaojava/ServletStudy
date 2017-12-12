26e4e11f0b8ae9d3f565a96da0fb63d6a619336c
wentaoxuerong <32974502+wentaoxuerong@users.noreply.github.com>
2017-12-12 22:54:12 +0800

    Servlet生命周期；ServletContext与ServletConfig

---
+++ servlet4/src/main/java/web/findEmpServlet.java
@@ -0,0 +1,21 @@
+package web;
+
+import java.io.IOException;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class findEmpServlet extends HttpServlet {
+
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		
+		ServletContext ctx=getServletContext();
+		String size=ctx.getInitParameter("size");
+		System.out.println(size);
+	}
+
+}
